76f3e9ff7100ae338bd458ebfebae62fe85a9506
Marta Susek <m.susek@live.com>
2023-06-12 08:39:12 +0200

    add defaultLocale to IntlPrivider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Provider store={store}>
       <ApolloProvider client={client}>
-        <IntlProvider locale="hr">
+        <IntlProvider locale="hr" defaultLocale='hr'>
           <SelectCoveragesPage />
         </IntlProvider>
       </ApolloProvider>
